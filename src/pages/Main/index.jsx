9ffacca03457982e7e19d38e844cc708f23eebef
rious275 <rious275@gmail.com>
2023-05-07 20:02:36 +0900

    메인화면 about 폰트 사이즈 변경

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -66,7 +66,7 @@ const Container = styled.div`
 
       .intro-text {
         max-width: 800px;
-        font-size: 2.2rem;
+        font-size: 1.8rem;
         font-weight: 300;
         text-align: center;
         line-height: 1.5;
