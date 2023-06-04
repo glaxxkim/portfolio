99b31e19bf8487f9ae9c6ddce32784568f5531fb
glaxxkim <131372838+glaxxkim@users.noreply.github.com>
2023-06-04 12:07:04 +0900

    Update event.js

--- src/assets/images/event.js
+++ src/assets/images/event.js
@@ -1,5 +1,107 @@
 const titles = [
-  'ROOKIEBUD 2023 S/S LOOKBOOK ', // 1
+  '2023 INVITATION TRAINING PROGRAM ', // 1
+  '2023 INVITATION TRAINING PROGRAM ',
+  '2023 INVITATION TRAINING PROGRAM ',
+  '2023 INVITATION TRAINING PROGRAM ',
+  '2023 INVITATION TRAINING PROGRAM ',
+  '2023 INVITATION TRAINING PROGRAM ',
+  '2023 INVITATION TRAINING PROGRAM ',
+  '2023 INVITATION TRAINING PROGRAM ',
+  '2023 INVITATION TRAINING PROGRAM ',
+  '2023 INVITATION TRAINING PROGRAM ', // 10
+  '2023 INVITATION TRAINING PROGRAM ',
+  '2023 INVITATION TRAINING PROGRAM ',
+  '2023 INVITATION TRAINING PROGRAM ',
+  '2023 INVITATION TRAINING PROGRAM ',
+  '2023 INVITATION TRAINING PROGRAM ',
+  '2023 INVITATION TRAINING PROGRAM ',
+  '2023 INVITATION TRAINING PROGRAM ',
+  '2023 INVITATION TRAINING PROGRAM ',
+  '2023 INVITATION TRAINING PROGRAM ',
+  '2023 INVITATION TRAINING PROGRAM ', // 20
+  '2023 INVITATION TRAINING PROGRAM ',
+  '2023 INVITATION TRAINING PROGRAM ',
+  '2023 INVITATION TRAINING PROGRAM ',
+  '2023 INVITATION TRAINING PROGRAM ',
+  '2023 INVITATION TRAINING PROGRAM ',
+  '2023 INVITATION TRAINING PROGRAM ',
+  '2023 INVITATION TRAINING PROGRAM ',
+  '2023 INVITATION TRAINING PROGRAM ',
+  '2023 INVITATION TRAINING PROGRAM ', // 29
+  'LIGETI FESTIVAL', // 30
+  'LIGETI FESTIVAL',
+  'LIGETI FESTIVAL',
+  'LIGETI FESTIVAL',
+  'LIGETI FESTIVAL',
+  'LIGETI FESTIVAL',
+  'LIGETI FESTIVAL',
+  'LIGETI FESTIVAL',
+  'LIGETI FESTIVAL',
+  'LIGETI FESTIVAL',
+  'LIGETI FESTIVAL', // 40
+  'LIGETI FESTIVAL',
+  'LIGETI FESTIVAL',
+  'LIGETI FESTIVAL',
+  'LIGETI FESTIVAL', //44
+  'Superpowers For Your Web3 Stack', //45
+  'Superpowers For Your Web3 Stack',
+  'Superpowers For Your Web3 Stack',
+  'Superpowers For Your Web3 Stack',
+  'Superpowers For Your Web3 Stack',
+  'Superpowers For Your Web3 Stack',
+  'Superpowers For Your Web3 Stack', // 50
+  'Superpowers For Your Web3 Stack',
+  'Superpowers For Your Web3 Stack',
+  'Superpowers For Your Web3 Stack',
+  'Superpowers For Your Web3 Stack',
+  'Superpowers For Your Web3 Stack',
+  'Superpowers For Your Web3 Stack',
+  'Superpowers For Your Web3 Stack',
+  'Superpowers For Your Web3 Stack',
+  'Superpowers For Your Web3 Stack', // 60
+  'Superpowers For Your Web3 Stack',
+  'Superpowers For Your Web3 Stack',
+  'Superpowers For Your Web3 Stack',
+  'Superpowers For Your Web3 Stack',
+  'Superpowers For Your Web3 Stack',
+  'Superpowers For Your Web3 Stack',
+  'Superpowers For Your Web3 Stack',
+  'Superpowers For Your Web3 Stack',
+  'Superpowers For Your Web3 Stack', // 70
+  'Superpowers For Your Web3 Stack',
+  'Superpowers For Your Web3 Stack',
+  'Superpowers For Your Web3 Stack',
+  'Superpowers For Your Web3 Stack',
+  'Superpowers For Your Web3 Stack',
+  'Superpowers For Your Web3 Stack',
+  'Superpowers For Your Web3 Stack',
+  'Superpowers For Your Web3 Stack',
+  'Superpowers For Your Web3 Stack', // 80
+  'Superpowers For Your Web3 Stack',
+  'Superpowers For Your Web3 Stack',
+  'Superpowers For Your Web3 Stack',
+  'Superpowers For Your Web3 Stack',
+  'Superpowers For Your Web3 Stack',
+  'Superpowers For Your Web3 Stack',
+  'Superpowers For Your Web3 Stack',
+  'Superpowers For Your Web3 Stack', // 89
+  '2023 한양대학교 공학대학원 9회 학술대회', // 90
+  '2023 한양대학교 공학대학원 9회 학술대회',
+  '2023 한양대학교 공학대학원 9회 학술대회',
+  '2023 한양대학교 공학대학원 9회 학술대회',
+  '2023 한양대학교 공학대학원 9회 학술대회',
+  '2023 한양대학교 공학대학원 9회 학술대회',
+  '2023 한양대학교 공학대학원 9회 학술대회',
+  '2023 한양대학교 공학대학원 9회 학술대회',
+  '2023 한양대학교 공학대학원 9회 학술대회',
+  '2023 한양대학교 공학대학원 9회 학술대회', // 100
+  '2023 한양대학교 공학대학원 9회 학술대회',
+  '2023 한양대학교 공학대학원 9회 학술대회',
+  '2023 한양대학교 공학대학원 9회 학술대회',
+  '2023 한양대학교 공학대학원 9회 학술대회',
+  '2023 한양대학교 공학대학원 9회 학술대회',
+  '2023 한양대학교 공학대학원 9회 학술대회', // 106
+];
 ];
 
 const BASE_URL =
