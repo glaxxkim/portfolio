35ef557f820ce289092704375057f487b1b6ea39
glaxxkim <131372838+glaxxkim@users.noreply.github.com>
2024-03-18 10:32:20 +0900

    Update guideList.js

--- src/pages/Guide/guideList.js
+++ src/pages/Guide/guideList.js
@@ -1,26 +1,26 @@
 export const guideList = [
   {
     title: '스냅촬영(1시간/원본 + 전체색보정)',
-    price: 100000,
+    price: 150000,
   },
   {
     title: '3시간(원본 + 정밀보정(4장)',
-    price: 300000,
+    price: 450000,
   },
   {
     title: '5시간(Halfday)(원본 + 정밀보정(8장)',
-    price: 500000,
+    price: 750000,
   },
   {
     title: '9시간(Fullday)(원본 + 정밀보정(12장)',
-    price: 900000,
+    price: 1350000,
   },
   {
     title: '사진 색보정(10장)',
-    price: 10000,
+    price: 20000,
   },
   {
     title: '사진 정밀보정(1장)',
-    price: 10000,
+    price: 20000,
   },
 ];
